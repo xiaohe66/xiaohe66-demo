e9c874b40c6a3c05b78ba1f9016e209b7788c091
xiaohe66 <ty@xiaohe66.com>
2019-11-22 11:06:23 +0800

    n%length == n&(length-1)

---
+++ xh-demo-arithmetic/src/main/java/com/xiaohe66/demo/arithmetic/math/DividedTwo.java
@@ -0,0 +1,55 @@
+package com.xiaohe66.demo.arithmetic.math;
+
+/**
+ * 若length 是2的 n 次方，则 n%length == n&(length-1)，而按位运算速度更快
+ *
+ * @author xiaohe
+ * @time 2019.11.22 10:31
+ */
+public class DividedTwo {
+
+    private static void test1(long n, int divisor) {
+        long a = n % divisor;
+        long b = n & (divisor - 1);
+        if (a != b) {
+            System.out.println(String.format("=%s,%s", a, b));
+        }
+    }
+
+    private static void test2(long n, int divisor) {
+        long a = n % divisor;
+    }
+
+    private static void test3(long n, int divisor) {
+        long b = n & (divisor - 1);
+    }
+
+
+    public static void main(String[] args) {
+
+        long n = 50L * 10000 * 10000;
+        int divisor = 8;
+
+//        for (int i = 0; i < n; i++) {
+//            test1(i, divisor);
+//        }
+
+        long time1 = System.currentTimeMillis();
+        for (long i = 0; i < n; i++) {
+            test2(i, divisor);
+        }
+        long time2 = System.currentTimeMillis();
+        for (long i = 0; i < n; i++) {
+            test3(i, divisor);
+        }
+        long time3 = System.currentTimeMillis();
+
+        long gap1 = time2 - time1;
+        long gap2 = time3 - time2;
+
+        System.out.println(String.format("取余花费:%s,按位花费:%s,按位是否更省时间：%s", gap1, gap2, gap1 > gap2));
+
+
+    }
+
+}
