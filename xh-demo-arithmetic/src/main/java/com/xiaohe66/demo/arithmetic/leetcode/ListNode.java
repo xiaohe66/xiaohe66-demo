cc1d27d3d35a88f19e62719b2aff18c1eb3aaa0a
xiaohe66 <ty@xiaohe66.com>
2020-10-13 17:48:10 +0800

    力扣

---
+++ xh-demo-arithmetic/src/main/java/com/xiaohe66/demo/arithmetic/leetcode/ListNode.java
@@ -0,0 +1,60 @@
+package com.xiaohe66.demo.arithmetic.leetcode;
+
+/**
+ * @author xiaohe
+ * @time 2020.10.13 14:00
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ListNode listNode = (ListNode) o;
+
+        if (val != listNode.val) return false;
+        return next != null ? next.equals(listNode.next) : listNode.next == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = val;
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append('[');
+
+        ListNode cur = this;
+
+        while (cur != null) {
+            if (cur != this) {
+                stringBuilder.append(',');
+            }
+            stringBuilder.append(cur.val);
+            cur = cur.next;
+        }
+        stringBuilder.append(']');
+        return stringBuilder.toString();
+    }
+}
