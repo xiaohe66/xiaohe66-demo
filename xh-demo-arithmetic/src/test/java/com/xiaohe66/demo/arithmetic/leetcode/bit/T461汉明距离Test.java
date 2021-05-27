f40cd2e63876c0e2f18e82135eaaf4f1b7b1d775
xiaohe66 <ty@xiaohe66.com>
2021-05-27 09:56:42 +0800

    力扣算法

---
+++ xh-demo-arithmetic/src/test/java/com/xiaohe66/demo/arithmetic/leetcode/bit/T461汉明距离Test.java
@@ -0,0 +1,17 @@
+package com.xiaohe66.demo.arithmetic.leetcode.bit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class T461汉明距离Test {
+
+    T461汉明距离 demo = new T461汉明距离();
+
+    @Test
+    public void test1() {
+
+        assertEquals(2, demo.hammingDistance(1, 4));
+
+    }
+}
