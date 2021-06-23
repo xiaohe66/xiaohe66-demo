606e32a973c39eb2d19ca26c2d48a4888cfeac6a
xiaohe66 <ty@xiaohe66.com>
2021-06-23 11:12:08 +0800

    力扣算法

--- xiaohe66-demo-arithmetic/src/test/java/com/xiaohe66/demo/arithmetic/leetcode/bit/T剑指Offer15二进制中1的个数Test.java
+++ xiaohe66-demo-arithmetic/src/test/java/com/xiaohe66/demo/arithmetic/leetcode/bit/T剑指Offer15二进制中1的个数Test.java
@@ -1,20 +1,19 @@
-package com.xiaohe66.demo.arithmetic.leetcode;
+package com.xiaohe66.demo.arithmetic.leetcode.bit;
 
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class T191位1的个数Test {
+public class T剑指Offer15二进制中1的个数Test {
 
-
-    T191位1的个数 demo = new T191位1的个数();
+    private T剑指Offer15二进制中1的个数 demo = new T剑指Offer15二进制中1的个数();
 
     @Test
     public void test1() {
 
+        assertEquals(32, demo.hammingWeight(-1));
 
         assertEquals(3, demo.hammingWeight(11));
-
 
         assertEquals(31, demo.hammingWeight(-3));
 
@@ -22,7 +21,6 @@ public class T191位1的个数Test {
         assertEquals(1, demo.hammingWeight(Integer.MIN_VALUE));
 
         assertEquals(16, demo.hammingWeight(-1431655766));
-
 
     }
 }
