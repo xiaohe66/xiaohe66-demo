0067748bf67a9b8eb1bc580d0e17a65ad8f4c6ff
xiaohe66 <ty@xiaohe66.com>
2021-09-01 15:19:38 +0800

    java 进制表示法

---
+++ xiaohe66-demo-se/src/main/java/com/xiaohe66/demo/se/base/DataType2.java
@@ -0,0 +1,24 @@
+package com.xiaohe66.demo.se.base;
+
+/**
+ * @author xiaohe
+ * @since 2021.08.31 16:58
+ */
+public class DataType2 {
+
+    public static void main(String[] args) {
+
+        // 2进制，0b开头
+        System.out.println(0b101);
+
+        // 8进制，0开头
+        System.out.println(011);
+
+        // 10进制，默认
+        System.out.println(11);
+
+        // 16进制，0x开头
+        System.out.println(0xAA);
+
+    }
+}
