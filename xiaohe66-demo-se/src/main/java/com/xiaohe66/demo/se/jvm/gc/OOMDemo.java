01e49fbeaadaa4cde2eeaa1bba4bf65025b8ba20
xiaohe66 <ty@xiaohe66.com>
2021-09-03 17:31:20 +0800

    OOMDemo

--- xiaohe66-demo-se/src/main/java/com/xiaohe66/demo/se/jvm/gc/OOMDemo.java
+++ xiaohe66-demo-se/src/main/java/com/xiaohe66/demo/se/jvm/gc/OOMDemo.java
@@ -8,6 +8,10 @@ import java.util.List;
  *
  * <p> 打开 VisualVM 中的插件 Visual GC 可以查看GC的过程
  *
+ * <p> Visual GC 插件需要安装
+ * 在 工具-插件设置-编辑里面，填入从 VisualVM 官网的插件中得到的url（在Plugins Centers中），即可安装
+ * 这里提供一个（不确定未来是否可用）：https://visualvm.github.io/archive/uc/8u40/updates.html
+ * <p>
  * // -Xms200M -Xmx200M -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=C:\\jvm.dump
  *
  * @author xiaohe
@@ -24,7 +28,7 @@ public class OOMDemo {
         int i = 0;
         while (true) {
             OOMDemo demo = new OOMDemo();
-            if(i++ % 20 == 0){
+            if (i++ % 5 > 0) {
                 list.add(demo);
             }
             // 调节该处的休眠时间以控制出现OOM的速度
